d7e7cfa402fcb2486e29c1ace52bb16fe004a4cd
EC2 Default User <ec2-user@ip-172-31-22-105.ec2.internal>
2021-06-05 02:52:00 +0000

    [Update] 2-4 エラー切り分け

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,17 @@
+class TodolistsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @list = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
